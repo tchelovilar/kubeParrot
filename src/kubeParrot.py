39b37750db6a8befe1fd7c50dfef6b3b4cf17d2d
Marcelo Silveira Vilar <tchelovilar@gmail.com>
2018-06-14 14:47:31 -0300

    syntax fix

--- src/kubeParrot.py
+++ src/kubeParrot.py
@@ -11,27 +11,27 @@ from modules.slackMessage import slackMessage
 print datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 #
-MONITOR_NAMESPACES=None
+MONITOR_NAMESPACES=[]
 if 'MONITOR_NAMESPACES' in os.environ:
-  MONITOR_NAMESPACES=os.environ['MONITOR_NAMESPACES'].split(",")
+    MONITOR_NAMESPACES=os.environ['MONITOR_NAMESPACES'].split(",")
 
 
 print "- Setup kubernetes client."
 config.load_incluster_config()
-kube = client.CoreV1Api()
-kubeAppApi = client.AppsV1Api()
+kube=client.CoreV1Api()
+kubeAppApi=client.AppsV1Api()
 
 
 print "- Setup Slack message Client."
 if 'SLACK_WEBHOOK' in os.environ:
-  SLACK_WEBHOOK=os.environ['SLACK_WEBHOOK']
+    SLACK_WEBHOOK=os.environ['SLACK_WEBHOOK']
 slack = slackMessage(SLACK_WEBHOOK)
 
 
 print "- Configuring Pod Monitor."
 POD_INFO_LEVEL=3
 if 'POD_INFO_LEVEL' in os.environ:
-  POD_INFO_LEVEL=os.environ['POD_INFO_LEVEL']
+    POD_INFO_LEVEL=os.environ['POD_INFO_LEVEL']
 configPodInformation={"level":POD_INFO_LEVEL,"namespaces": MONITOR_NAMESPACES}
 pod=podInformation(kube,slack,configPodInformation)
 
@@ -39,7 +39,7 @@ pod=podInformation(kube,slack,configPodInformation)
 print "- Configuring Deployment Monitor"
 DEPLOY_INFO_LEVEL=3
 if 'DEPLOY_INFO_LEVEL' in os.environ:
-  DEPLOY_INFO_LEVEL=os.environ['DEPLOY_INFO_LEVEL']
+    DEPLOY_INFO_LEVEL=os.environ['DEPLOY_INFO_LEVEL']
 configPodInformation={"level":DEPLOY_INFO_LEVEL,"namespaces": MONITOR_NAMESPACES}
 deploy=deploymentInformation(kubeAppApi,slack,configPodInformation)
 
@@ -47,7 +47,8 @@ deploy=deploymentInformation(kubeAppApi,slack,configPodInformation)
 print "- System started."
 count=1
 while True:
-  deploy.deployMonitor()
-  pod.podMonitor()
-  count+=1
-  time.sleep(3)
+    deploy.deployMonitor()
+    pod.podMonitor()
+    count+=1
+    time.sleep(3)
+  
--- src/modules/podInformation.py
+++ src/modules/podInformation.py
@@ -1,103 +1,102 @@
 import datetime
 
 class podInformation:
-  newPods=[]
+    newPods=[]
 
-  def __init__(self,kubeClient,slackClient,config=None):
-    self.lastInfo={}
-    self.count=1
-    self.kube=kubeClient
-    self.slack=slackClient
-    if not config:
-      self.config={"level":3,"namespaces": None}
-    else:
-      self.config=config
-
-
-  def podMonitor(self):
-    listPods=[]
-    if self.config["namespaces"]:
-      for namespace in self.config["namespaces"]:
-        listPods.extend(self.kube.list_namespaced_pod(namespace).items)
-    else:
-      listPods = self.kube.list_pod_for_all_namespaces(watch=False).items
-    # Run checks
-    self.containerCheck(listPods) # Conatiner Check need run before podCheck
-    self.podCheck(listPods)
-    self.podTerminatedCheck()
-    self.count+=1
+    def __init__(self,kubeClient,slackClient,config=None):
+        self.lastInfo={}
+        self.count=1
+        self.kube=kubeClient
+        self.slack=slackClient
+        if not config:
+            self.config={"level":3,"namespaces": []}
+        else:
+            self.config=config
 
 
-  def podCheck(self,listPods):
-    for pod in listPods:
-      if pod.metadata.uid in self.lastInfo:
-        if pod.status.phase != self.lastInfo[pod.metadata.uid].status.phase:
-          if pod.metadata.deletion_timestamp == None:
-            if (self.lastInfo[pod.metadata.uid].status.phase == "Pending"
-                  and pod.status.phase == "Running"
-                  and pod.metadata.uid in self.newPods
-                ):
-              self.log(3,"Created Pod: "+pod.metadata.name,"good",pod.metadata.namespace)
-              self.newPods.remove(pod.metadata.uid)
+    def podMonitor(self):
+        listPods=[]
+        if self.config["namespaces"]:
+            for namespace in self.config["namespaces"]:
+                listPods.extend(self.kube.list_namespaced_pod(namespace).items)
+        else:
+            listPods = self.kube.list_pod_for_all_namespaces(watch=False).items
+        # Run checks
+        self.containerCheck(listPods) # Conatiner Check need run before podCheck
+        self.podCheck(listPods)
+        self.podTerminatedCheck()
+        self.count+=1
+
+
+    def podCheck(self,listPods):
+        for pod in listPods:
+            if pod.metadata.uid in self.lastInfo:
+                if pod.status.phase != self.lastInfo[pod.metadata.uid].status.phase:
+                    if pod.metadata.deletion_timestamp == None:
+                        if (self.lastInfo[pod.metadata.uid].status.phase == "Pending"
+                                and pod.status.phase == "Running"
+                                and pod.metadata.uid in self.newPods
+                              ):
+                            self.log(3,"Created Pod: "+pod.metadata.name,"good",pod.metadata.namespace)
+                            self.newPods.remove(pod.metadata.uid)
+                        else:
+                            msg=("Pod %s status changed from %s to %s" %
+                                    (pod.metadata.name,
+                                      self.lastInfo[pod.metadata.uid].status.phase,
+                                      pod.status.phase ))
+                            self.log(2,msg,"good",pod.metadata.namespace)
+                    else:
+                        self.log(4,"Pod %s in termination process." % pod.metadata.name,"good",pod.metadata.namespace)
+                self.lastInfo[pod.metadata.uid]=pod
+                #print pod
+                self.lastInfo[pod.metadata.uid].checkNumber=self.count
             else:
-              msg=("Pod %s status changed from %s to %s" %
-                      (pod.metadata.name,
-                        self.lastInfo[pod.metadata.uid].status.phase,
-                        pod.status.phase ))
-              self.log(2,msg,"good",pod.metadata.namespace)
-
-          else:
-            self.log(4,"Pod %s in termination process." % pod.metadata.name,"good",pod.metadata.namespace)
-        self.lastInfo[pod.metadata.uid]=pod
-        #print pod
-        self.lastInfo[pod.metadata.uid].checkNumber=self.count
-      else:
-        self.lastInfo[pod.metadata.uid]=pod
-        self.lastInfo[pod.metadata.uid].checkNumber=self.count
-        if self.count > 1:
-          if pod.status.phase == "Running":
-            self.log(3,"Pod has been created: "+pod.metadata.name,"good",pod.metadata.namespace)
-          else:
-            # Add new pods in list to validate after if this pod go out of
-            # Pending Status.
-            self.newPods.append(pod.metadata.uid)
-            self.log(4,"New pod found: "+pod.metadata.name,"good",pod.metadata.namespace)
+                self.lastInfo[pod.metadata.uid]=pod
+                self.lastInfo[pod.metadata.uid].checkNumber=self.count
+                if self.count > 1:
+                    if pod.status.phase == "Running":
+                        self.log(3,"Pod has been created: "+pod.metadata.name,"good",pod.metadata.namespace)
+                    else:
+                        # Add new pods in list to validate after if this pod go out of
+                        # Pending Status.
+                        self.newPods.append(pod.metadata.uid)
+                        self.log(4,"New pod found: "+pod.metadata.name,"good",pod.metadata.namespace)
 
 
-  def podTerminatedCheck(self):
-    lastCheckNumber=self.count-1
-    terminatedPods=[]
-    for pod in self.lastInfo:
-      if self.lastInfo[pod].checkNumber == lastCheckNumber:
-        self.log(3,"Pod has been deleted: %s" % self.lastInfo[pod].metadata.name,"danger",self.lastInfo[pod].metadata.namespace)
-        terminatedPods.append(pod)
-    for pod in terminatedPods:
-      del self.lastInfo[pod]
+    def podTerminatedCheck(self):
+        lastCheckNumber=self.count-1
+        terminatedPods=[]
+        for pod in self.lastInfo:
+            if self.lastInfo[pod].checkNumber == lastCheckNumber:
+                self.log(3,"Pod has been deleted: %s" % self.lastInfo[pod].metadata.name,"danger",self.lastInfo[pod].metadata.namespace)
+                terminatedPods.append(pod)
+        for pod in terminatedPods:
+            del self.lastInfo[pod]
 
 
-  def containerCheck(self,listPods):
-    for pod in listPods:
-      if pod.metadata.uid in self.lastInfo:
-        lastInfoPod=self.lastInfo[pod.metadata.uid]
-        i=0
-        for container in pod.status.container_statuses:
-          if container.restart_count > lastInfoPod.status.container_statuses[i].restart_count:
-            self.log(1,"Container *%s* from Pod *%s* has been restarted" % (container.name,pod.metadata.name),
-                     "danger",pod.metadata.namespace
-                    )
-          i+=1
+    def containerCheck(self,listPods):
+        for pod in listPods:
+            if pod.metadata.uid in self.lastInfo:
+                lastInfoPod=self.lastInfo[pod.metadata.uid]
+                i=0
+                for container in pod.status.container_statuses:
+                    if container.restart_count > lastInfoPod.status.container_statuses[i].restart_count:
+                        self.log(1,"Container *%s* from Pod *%s* has been restarted" % (container.name,pod.metadata.name),
+                              "danger",pod.metadata.namespace
+                              )
+                    i+=1
 
 
-  def log(self,level,message,type="good",namespace=""):
-    date=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print ("%s Namespace: %s  Msg: %s" % (date, namespace, message))
-    if level <= self.config["level"]:
-      payload={
-           "username": "kube-info",
-           "attachments":[ {
-                 "color":type,
-                 "title":"Namespace: "+namespace,
-                 "text":message,
-            } ]
-      }
-      self.slack.sendMessage(payload)
+    def log(self,level,message,type="good",namespace=""):
+        date=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print ("%s Namespace: %s  Msg: %s" % (date, namespace, message))
+        if level <= self.config["level"]:
+            payload={
+                "username": "kube-info",
+                "attachments":[ {
+                      "color":type,
+                      "title":"Namespace: "+namespace,
+                      "text":message,
+                  } ]
+            }
+            self.slack.sendMessage(payload)
